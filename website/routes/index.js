bf90ed2fb668264b49b41ae8b4b3f3efbc16bb2b
Darya Murauyova <muravkina@yahoo.com>
2015-10-17 10:01:58 -0400

    authorization

--- website/routes/index.js
+++ website/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var Review = require("./../models/reviews");
 var Blog = require("./../models/blog");
 var User = require("./../models/user");
@@ -11,7 +12,8 @@ router.get('/', function(req, res, next) {
       console.log("db error in GET /posts: " + err);
       res.render('error');
     } else {
-      res.render('index', {reviews: reviews});
+      console.log(req.user)
+      res.render('index', {reviews: reviews, user: req.user});
     };
   });
 });
@@ -22,8 +24,33 @@ router.post('/', function(req, res, next){
   })
 })
 router.get('/login', function(req, res, next){
-  res.render('login', {})
+  res.render('login', {user: req.user})
 })
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+router.post('/register', function(req, res) {
+    console.log(req.body)
+    User.register(new User({ name: req.body.name, username: req.body.username }), req.body.password, function(err, user) {
+        if (err) {
+            return res.render('register', { user : user });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+          console.log("success")
+            res.redirect('/');
+        });
+    });
+});
 
 
 module.exports = router;
