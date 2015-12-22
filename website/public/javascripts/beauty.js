dbcdb953d83ea628fbf4cca3e9825d219da96601
Darya Murauyova <muravkina@yahoo.com>
2015-12-22 17:29:28 -0500

    new main page layout, blog responsivness

--- website/public/javascripts/beauty.js
+++ website/public/javascripts/beauty.js
@@ -25,6 +25,54 @@ $(document).ready(function() {
   });
   }
 
+  $("#path3397").on("click", function(){
+    console.log("hello")
+  })
+
   $(window).on('scroll resize', check_if_in_view);
+
+  var displayWord = function(event){
+    var topPart = $(this).find("img.top_part");
+    var middlePart = $(this).find("img.middle_part");
+    var bottomPart = $(this).find("img.bottom_part");
+    var word = $(this).find("h2");
+
+    topPart.hide("slide", { direction: "left" }, 200);
+    middlePart.hide("slide", { direction: "right" }, 200);
+    word.show("slide", {direction: "left"}, 200);
+    bottomPart.hide("slide", { direction: "left" }, 200);
+  }
+
+  var hideWord = function(){
+    var topPart = $(this).find("img.top_part");
+    var middlePart = $(this).find("img.middle_part");
+    var bottomPart = $(this).find("img.bottom_part");
+    var word = $(this).find("h2");
+
+    topPart.show("slide", { direction: "left" }, 200);
+    middlePart.show("slide", { direction: "right" }, 200);
+    word.hide("slide", {direction: "left"}, 200);
+    bottomPart.show("slide", { direction: "left" }, 200);
+  }
+
+  var displayContent = function(){
+    var topPart = $(".section").find("img.top_part");
+    var middlePart = $(".section").find("img.middle_part");
+    var bottomPart = $(".section").find("img.bottom_part");
+
+    topPart.hide("slide", { direction: "left" }, 200);
+    middlePart.hide("slide", { direction: "right" }, 200);
+    bottomPart.hide("slide", { direction: "left" }, 200);
+
+    $(".section").off("mouseover").off("mouseleave")
+
+  }
+
+
+
+  $(".section").on("mouseover", displayWord);
+  $(".section").on("mouseleave", hideWord);
+  $(".about_image").on("click", "h2", displayContent);
+
 
 })
